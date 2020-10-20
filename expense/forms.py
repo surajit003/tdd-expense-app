e2f701800ac6a69ee050da7161cb9b0c11e90c16
surajit <surajit@poweredbypeople.io>
2020-10-21 01:23:31 +0300

    added modelform

---
+++ expense/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import Expense
+
+
+class ExpenseForm(ModelForm):
+    class Meta:
+        model = Expense
+        exclude = ("total", "updated_at", "created_at", "expense_id")
