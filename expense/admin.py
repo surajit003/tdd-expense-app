e1e7b4fef8becce99af1083a01d7bc532fbc1964
surajit <surajit@poweredbypeople.io>
2020-10-21 00:58:51 +0300

    added modeladmin

--- expense/admin.py
+++ expense/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Expense
 
 # Register your models here.
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ("expense_id", "total", "updated_at")
