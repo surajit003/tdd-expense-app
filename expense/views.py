3c8aae1e5a39ef1c859ec8a550e4844522786578
surajit <surajit@poweredbypeople.io>
2020-10-21 01:32:56 +0300

    added post handler and form errors

--- expense/views.py
+++ expense/views.py
@@ -8,3 +8,8 @@ def HomeView(request):
     if request.method == "GET":
         form = ExpenseForm()
         return render(request, "expense/home.html", {"form": form})
+    if request.method == "POST":
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return render(request, "expense/home.html", {"form": form})
