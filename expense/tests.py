a60b7949d498ec828c326022641921a7b470e303
surajit <surajit@poweredbypeople.io>
2020-10-21 00:29:23 +0300

    added test for homepage returning template

--- expense/tests.py
+++ expense/tests.py
@@ -1,11 +1,22 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
+from django.http import HttpRequest
 from .views import HomeView
+from django.test.client import Client
 
 # Create your tests here.
 
 
 class HomePageTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve(reverse("expense:home"))
         self.assertEqual(found.func, HomeView)
+
+    def test_home_page_returns_correct_template(self):
+        url = reverse("expense:home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "expense/home.html")
