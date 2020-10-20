94f3e5ced2e407618b23c3a148aaa3dd8ad24437
surajit <surajit@poweredbypeople.io>
2020-10-21 00:02:21 +0300

    added home url and view

---
+++ expense/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+app_name = "expense"
+
+urlpatterns = [
+    url(
+        r"home/$",
+        views.home,
+        name="home",
+    ),
+]
--- expense/views.py
+++ expense/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def HomeView(request):
+    if request.method=='GET':
+        return render(request,'expense/home.html')
