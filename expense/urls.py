33187d1b31bf18d2d4c127e9559d9ac647c0c72e
surajit <surajit@poweredbypeople.io>
2020-10-21 02:10:40 +0300

    added detail endpoint

--- expense/urls.py
+++ expense/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 app_name = "expense"
 
-urlpatterns = [url(r"home/$", views.HomeView, name="home")]
+urlpatterns = [
+    url(r"home/$", views.HomeView, name="home"),
+    url(
+        r"^(?P<expense_id>[0-9a-f-]+)/detail/$",
+        views.ExpenseDetail,
+        name="expense_detail",
+    ),
+]
--- expense/views.py
+++ expense/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ExpenseForm
+from .models import Expense
+from django.http import HttpResponseNotFound
+
 
 # Create your views here.
 
@@ -11,5 +14,15 @@ def HomeView(request):
     if request.method == "POST":
         form = ExpenseForm(request.POST)
         if form.is_valid():
-            form.save()
+            instance = form.save()
+            return redirect("expense:expense_detail", expense_id=instance.expense_id)
         return render(request, "expense/home.html", {"form": form})
+
+
+def ExpenseDetail(request, expense_id):
+    if request.method == "GET":
+        expense = Expense.objects.filter(expense_id=expense_id)
+        if expense:
+            return render(request, "expense/detail.html", {"expense": expense})
+        else:
+            return HttpResponseNotFound("No expense found with those details")
