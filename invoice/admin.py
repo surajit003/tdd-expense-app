dc2be55a86ce1df0cd8713c3ec3a69e59f5846f9
surajit <surajit@poweredbypeople.io>
2020-11-08 23:41:07 +0300

    add model admin

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Invoice
+from .models import Invoice, Company, TaxSetUp, BaseSalarySetUp
 
 # Register your models here.
 
@@ -7,3 +7,18 @@ from .models import Invoice
 @admin.register(Invoice)
 class InvoiceModelAdmin(admin.ModelAdmin):
     list_display = ("company", "base_pay", "net_amount", "user", "created_at")
+
+
+@admin.register(Company)
+class CompanyModelAdmin(admin.ModelAdmin):
+    list_display = ("name", "email")
+
+
+@admin.register(TaxSetUp)
+class TaxSetUpModelAdmin(admin.ModelAdmin):
+    list_display = ("country", "tax")
+
+
+@admin.register(BaseSalarySetUp)
+class BaseSalarySetUpModelAdmin(admin.ModelAdmin):
+    list_display = ("company", "base_salary", "tax")
