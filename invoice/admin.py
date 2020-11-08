998b7766f0567ad311c3958a2c080cd8fd5bb180
surajit <surajit@poweredbypeople.io>
2020-11-08 23:26:34 +0300

    add modeladmin

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Invoice
 
 # Register your models here.
+
+
+@admin.register(Invoice)
+class InvoiceModelAdmin(admin.ModelAdmin):
+    list_display = ("company", "base_pay", "net_amount", "user", "created_at")
